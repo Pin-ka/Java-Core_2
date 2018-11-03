dff28b4deb293315be8590004a5d21a6328fb71a
Pin-ka <pin-ka@outlook.com>
2018-11-04 03:35:53 +0500

    Выполнено задание к уроку 4: написан прототип чата

---
+++ Lesson_4/JavaFX_Chat/src/sample/Controller.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+
+public class Controller {
+
+    @FXML
+    ListView listView;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    HBox controlPane;
+
+    @FXML
+    ImageView sendButton;
+
+    public void sendMsg(ActionEvent actionEvent) {
+
+        sendMessage();
+
+    }
+
+    public void sendMessage() {
+        Label message = new Label(textField.getText());
+
+        VBox messageBox = new VBox(message);
+
+        if (listView.getItems().size()%2 != 0) messageBox.setAlignment(Pos.CENTER_RIGHT);
+
+        listView.getItems().add(messageBox);
+        textField.clear();
+        textField.requestFocus();
+        listView.scrollTo(listView.getItems().size()-1);
+    }
+}
