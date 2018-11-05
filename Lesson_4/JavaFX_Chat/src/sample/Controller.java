f46c90f2480bccbd3c58c88c85895783a9b9d549
Pin-ka <pin-ka@outlook.com>
2018-11-05 15:06:23 +0500

    Выполнено задание к уроку 4: написан прототип чатаСоздано окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна. Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели. Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter. При «отсылке» сообщение перекидывается из нижнего поля в центральное. Оформлено с помощью css

--- Lesson_4/JavaFX_Chat/src/sample/Controller.java
+++ Lesson_4/JavaFX_Chat/src/sample/Controller.java
@@ -3,10 +3,10 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -23,7 +23,7 @@ public class Controller {
     HBox controlPane;
 
     @FXML
-    ImageView sendButton;
+    Button send;
 
     public void sendMsg(ActionEvent actionEvent) {
 
